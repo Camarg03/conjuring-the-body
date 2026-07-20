809c4c255a1ef3aa4a868b4382a363a42e5e8355
lole <lol@loles-MacBook-Pro.local>
2026-07-20 21:09:08 +0200

    Quartz sync: Jul 20, 2026, 9:09 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -1,14 +1,29 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import style from "./styles/footer.scss"
+import { version } from "../../package.json"
+import { i18n } from "../i18n"
 
-export default (() => {
-  const Footer: QuartzComponent = ({ displayClass }: QuartzComponentProps) => {
+interface Options {
+  links: Record<string, string>
+}
+
+export default ((opts?: Options) => {
+  const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
+    const year = new Date().getFullYear()
+    const links = opts?.links ?? []
     return (
       <footer class={`${displayClass ?? ""}`}>
-        <div class="cbk-footer-credit">
-          <img src="/conjuring-the-body/assets/cbk_logo.png" alt="CBK Rotterdam" />
-          <p>Made possible with a financial contribution from CBK Rotterdam.</p>
-        </div>
+        <p>
+          {i18n(cfg.locale).components.footer.createdWith}{" "}
+          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+        </p>
+        <ul>
+          {Object.entries(links).map(([text, link]) => (
+            <li>
+              <a href={link}>{text}</a>
+            </li>
+          ))}
+        </ul>
       </footer>
     )
   }
