07c3db05b71fca4351b464387fe2da21d7efb814
lole <lol@loles-MacBook-Pro.local>
2026-07-20 21:12:48 +0200

    Quartz sync: Jul 20, 2026, 9:12 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -13,6 +13,11 @@ export default ((opts?: Options) => {
     const links = opts?.links ?? []
     return (
       <footer class={`${displayClass ?? ""}`}>
+        <div class="cbk-footer-credit">
+          <img src="/conjuring-the-body/assets/cbk_logo.png" alt="CBK Rotterdam" />
+          <p>Made possible with a financial contribution from CBK Rotterdam.</p>
+        </div>
+
         <p>
           {i18n(cfg.locale).components.footer.createdWith}{" "}
           <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
